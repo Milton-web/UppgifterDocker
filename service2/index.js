5c8c6098e45a4c25da6689fc306ed640a0d614a8
Milton-web <milton.tuti@chasacademy.se>
2025-10-27 15:50:29 +0100

    added v36 uppgift 3

---
+++ service2/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from service 2!');
+});
+
+
+ app.listen(PORT, () => {
+ console.log(`Server running on: http://localhost:${PORT}`);
+});
